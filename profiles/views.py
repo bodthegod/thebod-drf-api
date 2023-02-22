891f80de1ca202930402f2f9847ef12d19ddf631
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-22 14:04:14 +0000

    Create put request to edit and update profile data

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -11,6 +12,10 @@ class ProfileList(APIView):
     """
 
     def get(self, request):
+        """
+        Get request to retrieve all profile objects
+        and return serialized data
+        """
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
@@ -20,8 +25,12 @@ class ProfileDetail(APIView):
     """
     Display details of specific profile based on primary key
     """
-
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
+        """
+        Get request to retrieve single profile object
+        based upon id
+        """
         try:
             profile = Profile.objects.get(pk=pk)
             return profile
@@ -29,6 +38,20 @@ class ProfileDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
+        """
+        Gets and returns serialized data
+        """
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Put method to allow for editing of profile, name, content, image
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

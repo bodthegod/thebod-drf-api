d81d8867163a2868a5a18735ade61c12616bce6b
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-22 16:09:49 +0000

    Fix non nullable field bug

--- posts/models.py
+++ posts/models.py
@@ -2,24 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Tags(models.Model):
-    """
-    Model for tags associated to posts
-    """
-    tags = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f'{self.tags}'
-
-
 class Post(models.Model):
     """
     Post Model
     """
+    # tags_array = [
+    #     ('Bodybuilding', 'BodyBuilding'),
+    #     ('Running', 'Running'),
+    #     ('Sports', 'Sports'),
+    #     ('Fitness', 'Fitness'),
+    #     ('Wellbeing', 'Wellbeing'),
+    #     ('Bodybuilding', 'BodyBuilding'),
+    # ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
+    # tags = models.CharField(max_length=20, choices=tags_array)
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_f3qkbi', blank=True

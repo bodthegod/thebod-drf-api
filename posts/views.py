81ceef974ac735cb7a6fb9204c075fa06861fad6
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-22 15:42:16 +0000

    Create postlist view and move api-auth path

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    """
+    Postlist class to display all posts
+    """
+
+    def get(self, request):
+        """
+        Get request to get all posts
+        """
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)

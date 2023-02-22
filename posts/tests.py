1556373aaaf91320b2acfaded14255bdf1bbb896
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-22 16:50:29 +0000

    Create postlistviewtests and tests listable posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    """
+    Tests for postlistview
+    """
+    def setUp(self):
+        User.objects.create_user(username='joe', password='password')
+
+    def test_can_list_posts(self):
+        """
+        Tests if a user can list any post
+        """
+        joe = User.objects.get(username='joe')
+        Post.objects.create(owner=joe, title='joes title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+

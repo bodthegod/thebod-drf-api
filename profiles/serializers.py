439a679ff559444249d9010da31096a38330cea2
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 20:03:02 +0000

    Add following_id get request in serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from followers.models import Follower
 from .models import Profile
 
 
@@ -16,6 +17,17 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        """
+        Get following id request
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            print(following)
+            
     class Meta:
         """
         Profile model fields
@@ -23,5 +35,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner',
+            'content', 'image', 'is_owner', 'following_id'
         ]

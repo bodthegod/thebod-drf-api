be853664ea57a0e62aa91adfa99789a199e218de
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 16:48:18 +0000

    Create serializer for follower model

---
+++ followers/serializers.py
@@ -0,0 +1,28 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializes data for followers
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_username = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        """
+        Fields associated with follower
+        """
+        model = Follower
+        fields = ['id', 'owner', 'post', 'created_at', 'followed_username']
+
+    def create(self, validated_data):
+        """
+        Error handling for duplicated follow result (only one follow per user)
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {'detail': 'duplicated follow, one follow per user'})

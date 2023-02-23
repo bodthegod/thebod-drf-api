98facddaa716e0004f0892b32189a8549f5f23a0
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 13:04:59 +0000

    Create commentserializer

---
+++ comments/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Class for comment serializer
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        """
+        Get request for owner of comment
+        """
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        """
+        Comment model fields
+        """
+        model = Comment
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'profile_id',
+            'profile_image', 'is_owner', 'post', 'comment_info',
+        ]

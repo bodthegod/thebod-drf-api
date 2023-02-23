da05a57b30d47c65e3059b17c47449e7749e57b5
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 13:03:22 +0000

    Create comments app and add comments model

---
+++ comments/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class Comment(models.Model):
+    """
+    Comment class for content related to comments
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_info = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Ordering of commments
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns comment information
+        """
+        return self.comment_info

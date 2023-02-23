4bacf951441080323ae2b3cd4494634aef9a33c1
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 16:53:00 +0000

    Create views for follower list and details

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from thebod_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List followers of a user
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get individual follower details, delete or add by follower id
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()

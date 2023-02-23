fc7fc57f1aecd53094a1de0988871c7144715909
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-23 16:58:06 +0000

    Refactor profiles views with generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,63 +1,21 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from thebod_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
     """
-
-    def get(self, request):
-        """
-        Get request to retrieve all profile objects
-        and return serialized data
-        """
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request})
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    """
-    Display details of specific profile based on primary key
-    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Display details of specific profile based on primary key (has to be owner)
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        """
-        Get request to retrieve single profile object
-        based upon id, checks if is owner
-        """
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        """
-        Gets and returns serialized data
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """
-        Put method to allow for editing of profile, name, content, image
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer

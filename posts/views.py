0f7a8e7c35b5735b7d59a0032cb1175bfd432d0b
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-24 14:19:33 +0000

    Add search for username and title to posts

--- posts/views.py
+++ posts/views.py
@@ -17,7 +17,12 @@ class PostList(generics.ListCreateAPIView):
         comments_total=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'title',
+        'owner__username'
     ]
     ordering_fields = [
         'comments_total',

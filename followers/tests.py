02681be42d54deda80e999037cf87a66015d9aa0
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-02-26 11:15:10 +0000

    Create automated test for followerlist

--- followers/tests.py
+++ followers/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Follower
 
-# Create your tests here.
+
+class FollowerListViewTests(APITestCase):
+    """
+    Tests for followerlistview
+    """
+
+    def setUp(self):
+        User.objects.create_user(username='joe', password='password')
+
+    def test_unauth_prevent_follow(self):
+        """
+        Tests if an unauthenticated user cant follow
+        """
+        response = self.client.get('/followers/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)

5da83bba43dd1b1a085b1698ec673d4bb21e3000
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-07 18:23:05 +0000

    Finalise automated testing for ProfileDetail view

--- profiles/tests.py
+++ profiles/tests.py
@@ -5,6 +5,9 @@ from .models import Profile
 
 
 class ProfileDetailViewTests(APITestCase):
+    """
+    Tests for specific user methods tied to a profile
+    """
     def setUp(self):
         """
         Creates two user objects for the setup
@@ -45,3 +48,11 @@ class ProfileDetailViewTests(APITestCase):
         self.client.login(username='joe', password='joespassword')
         response = self.client.get('/profiles/200/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_unauthenticated_user_cant_edit_profile(self):
+        """
+        Test if an unauthenticated user can't edit a profile
+        """
+        response = self.client.put('/profiles/2/', {'content': 'i still cant edit :('})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+

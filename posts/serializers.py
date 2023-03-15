eca500646a765672960d3b89c8a1f2cca2d2435a
bodthegod <77025894+bodthegod@users.noreply.github.com>
2023-03-15 14:58:12 +0000

    Edit: Post file validation checks

--- posts/serializers.py
+++ posts/serializers.py
@@ -23,17 +23,15 @@ class PostSerializer(serializers.ModelSerializer):
         Validates images by checking file size/height/width
         and displays error if too large
         """
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width is wider than 4096px'
-            )
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image height is more than 4096px'
+                'Image height larger than 4096px!'
             )
-        if value.size > 1024 * 1024 * 4:
+        if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image size is larger than 4MB'
+                'Image width larger than 4096px!'
             )
         return value
 
